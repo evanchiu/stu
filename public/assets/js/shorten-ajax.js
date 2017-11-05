dc14233673ebd8d7dc9e0aa7e7f80159eca84139
Evan Chiu <evan@evanchiu.com>
2017-11-04 22:02:57 -0700

    Updating front end to post to /create

---
+++ public/assets/js/shorten-ajax.js
@@ -0,0 +1,15 @@
+function shorten() {
+  var longUrl = document.getElementById('long-url').value;
+  fetch("/create", {
+    method: 'POST',
+    body: JSON.stringify({longUrl: longUrl})
+  }).then(function(response) {
+    if (response.ok) {
+      return response.json();
+    }
+  }).then(function(json) {
+    document.getElementById('short-url').innerHTML = '<a href = "' + json.shortUrl + '">' + json.shortUrl + '</a>';
+  }).catch(function(error) {
+    document.getElementById('short-url').innerHTML = 'Error shortening: ' + error;
+  });
+}
