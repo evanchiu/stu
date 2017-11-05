114731efd0569a944c4680ea71b7c3b66c7468de
Evan Chiu <evan@evanchiu.com>
2017-11-04 23:13:34 -0700

    Storing created urls in a DynamoDB table

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
-var fs = require('fs');
+
+const aws = require('aws-sdk');
+const dynamodb = new aws.DynamoDB();
+
+const crypto = require('crypto');
+const fs = require('fs');
 
 // Map of routes to functions
 var routes = {
   '/': index
+  '/create': create
 }
 
 // Handler provides routing
@@ -20,6 +26,59 @@ exports.handler = function(event, context, callback) {
 function index(event, context, callback) {
   var contents = fs.readFileSync("public/index.html");
   done(200, contents.toString(), 'text/html', callback);
+}
+
+// Create a new teeny url
+function create(event, context, callback) {
+  var json;
+  if (!event.body) {
+    return done(400, '{"error": "Missing body"}', 'application/json', callback);
+  }
+  try {
+    json = JSON.parse(event.body);
+  } catch (e) {
+    return done(400, '{"error": "Invalid JSON body"}', 'application/json', callback);
+  }
+
+  shortSave(longUrl, function(err, data) {
+    if (err) {
+      return done(500, '{"error": "Internal Server Error"}', 'application/json', callback);
+    } else {
+      return done(200, data.toString(), 'application/json', callback);
+    }
+  }
+}
+
+// Generate the short path for the given url and store it in the database
+function shortSave(longUrl, callback) {
+  var shortUrl = shorten(longUrl);
+  var params = {
+    TableName: process.env.REDIRECTS_TABLE,
+    Item: {
+      shortUrl: shortUrl,
+      data: {
+        longUrl: longUrl,
+        createDate: new Date()
+      }
+    }
+  }
+
+  dynamodb.putItem(params, function(err, data) {
+    if (err) {
+      console.error('DyanmoDB error on save: ', err);
+      return callback(err);
+    } else {
+      return callback(null, JSON.stringify({shortUrl: shortUrl}));
+    }
+  }
+}
+
+// For this demo, we're just going to use the first 7 base-64 encoded characters of the sha256 hash
+// This gives us about 4 trillion possibilities
+function shorten(url) {
+  var hash = crypto.createHash('sha256');
+  hash.update(url);
+  return hash.digest('base64').substring(0, 7);
 }
 
 // We're done with this lambda, return to the client with given parameters
